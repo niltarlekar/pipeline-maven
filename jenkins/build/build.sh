60af7a29609811410aee0d0e3bade053fdb64393
jenkins <jenkins@localhost.localdomain>
2021-05-14 02:18:38 -0400

    corrected build.sh

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -7,4 +7,4 @@ echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-mvn
-cd $WORKSPACE/jenkins/build && docker-compose -f docker-compose-build.yml build --no-cache
+cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
