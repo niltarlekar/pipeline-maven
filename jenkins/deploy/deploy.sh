988e04fd77a70da9f438a67ff3155abd54d24a25
jenkins <jenkins@localhost.localdomain>
2021-05-14 02:25:46 -0400

    correct key file path

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,6 +5,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /home/jenkins/prod /tmp/.auth produser@192.168.0.115:/tmp/.auth
-scp -i /home/jenkins/prod  ./jenkins/deploy/publish produser@192.168.0.115:/tmp/publish
-ssh -i /home/jenkins/prod produser@192.168.0.115 "/tmp/publish"
+scp -i /opt/prod /tmp/.auth produser@192.168.0.115:/tmp/.auth
+scp -i /opt/prod  ./jenkins/deploy/publish produser@192.168.0.115:/tmp/publish
+ssh -i /opt/prod produser@192.168.0.115 "/tmp/publish"
